468ac1349095ced44d8c737dc0e535e70df98019
jaleeson11 <66011869+jaleeson11@users.noreply.github.com>
2022-09-11 17:53:58 +0100

    Updated ball starting position

--- src/ball.js
+++ src/ball.js
@@ -5,7 +5,7 @@ export default class Ball {
 
 		this.game = game;
 
-		this.position = {x: 10, y: 50};
+		this.position = {x: 0, y: 200};
 		this.speed = {x: 4, y: 4};
 		this.size = 16;
 
@@ -18,7 +18,7 @@ export default class Ball {
 	}
 
 	reset() {
-		this.position = {x: 10, y: 50};
+		this.position = {x: 0, y: 200};
 	}
 
 	update(deltaTime) {
